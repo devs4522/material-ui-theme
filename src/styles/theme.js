fd2d5172d1e7a4c45139e0a53ad87d4c6fdcf822
Devendra <devendra.singh@tarento.com>
2021-07-28 19:39:02 +0530

    Primary and secondary color is updated in the theme

--- src/styles/theme.js
+++ src/styles/theme.js
@@ -1,7 +1,12 @@
 import { createTheme }  from '@material-ui/core/styles'
 const theme = createTheme({
   palette: {
-    primary: { 500: '#467fcf' },
+    primary: {
+         main: '#311b92' 
+    },
+    secondary: {
+        main: '#7e57c2'
+    }
   },
 })
 export default theme
